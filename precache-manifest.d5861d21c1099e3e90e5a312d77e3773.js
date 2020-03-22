4ac1d2ebdf9992e14d7fece2eddb15f5c36a39e6
Robin Lindner <robin@deeprobin.de>
2020-03-23 00:44:29 +0100

    Added built files

---
+++ precache-manifest.d5861d21c1099e3e90e5a312d77e3773.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abbabe60c193cee1144c",
+    "url": "/css/about.8c21e62f.css"
+  },
+  {
+    "revision": "ded416f9305e3ccee26f",
+    "url": "/css/app.a25583bd.css"
+  },
+  {
+    "revision": "f711a4526e739e55f7b0",
+    "url": "/css/chunk-39b2c54a.c6d545f8.css"
+  },
+  {
+    "revision": "93c86787197a10379d43",
+    "url": "/css/chunk-6d8ed82c.b8e28bd9.css"
+  },
+  {
+    "revision": "1c67ef93c51b5cfc5ca5",
+    "url": "/css/chunk-af411f78.79d9169d.css"
+  },
+  {
+    "revision": "f80c83c92b79c1ca878e17bdb8c5dff5",
+    "url": "/img/harold.f80c83c9.png"
+  },
+  {
+    "revision": "45a66496dc84742691e67d440dbf3780",
+    "url": "/img/wirvsvirus.45a66496.svg"
+  },
+  {
+    "revision": "3cbcb6aa831c3bf383494984bcccd615",
+    "url": "/img/wirvsvirus_alt.3cbcb6aa.png"
+  },
+  {
+    "revision": "e33dc7b952d8d300e0645e268ee9a097",
+    "url": "/img/wirvsvirus_white.e33dc7b9.svg"
+  },
+  {
+    "revision": "affb7653d4a198cbb87498dc637e1f16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abbabe60c193cee1144c",
+    "url": "/js/about.be363165.js"
+  },
+  {
+    "revision": "ded416f9305e3ccee26f",
+    "url": "/js/app.96fb84c4.js"
+  },
+  {
+    "revision": "f711a4526e739e55f7b0",
+    "url": "/js/chunk-39b2c54a.f117b3a3.js"
+  },
+  {
+    "revision": "93c86787197a10379d43",
+    "url": "/js/chunk-6d8ed82c.e1d2efc3.js"
+  },
+  {
+    "revision": "1c67ef93c51b5cfc5ca5",
+    "url": "/js/chunk-af411f78.91c78b6f.js"
+  },
+  {
+    "revision": "5256e0d94f0c17bb0246",
+    "url": "/js/chunk-vendors.9ad61235.js"
+  },
+  {
+    "revision": "4b14c64efaf846819b9a229b4193c8b7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
